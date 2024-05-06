4f6999b4829c0b58b4457da0004864d01dba5f04
kushagra-devtron <126449022+kushagra-devtron@users.noreply.github.com>
2024-05-06 13:30:51 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello, World from Docker! <br>";
+echo "Hello, World from Docker! Adding this string for testing<br>";
 echo "Hello ECS! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
